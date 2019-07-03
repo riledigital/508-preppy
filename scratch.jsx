c807e342e9476218c61b19ffe17b4d72e59a8b38
rile <r_le@icloud.com>
2019-07-03 10:58:25 -0400

    added metadata function and created scratch file

---
+++ scratch.jsx
@@ -0,0 +1,57 @@
+
+
+// BEFORE
+/* ------------------------------
+[No Paragraph Style]
+[Basic Paragraph]
+Slide Captions
+Normal
+Multi-level List 1
+Multi-level List 2
+Heading 1
+slide caption
+box
+List Bullet
+Heading 2.0
+Block Text1
+Body Text
+Default
+Heading 2
+Heading 3
+List Paragraph
+Block Text3
+------------------------------ */
+
+// AFTER 
+/* ------------------------------
+[No Paragraph Style]
+[Basic Paragraph]
+Aside Box - Body
+Aside Box - Heading
+Body Text (Base)
+Question
+Checklist
+Checklist - Checked
+Fill-in Ruled
+Fill-in Ruled with Checkboxes
+Heading Lv 1
+Heading Lv 2
+Heading Lv 3
+Definition List - Definition
+Definition List - Description
+List Bullet
+List Bullet - Small Text
+List Numbered
+Running Header - Document Title
+Footer - Page Num and Date
+Tabular Body
+Tabular - Numbers, Right-Aligned
+Tabular Heading
+Cover Title
+TOC Listing Level 1
+TOC Listing Level 2
+Caption
+Reference Item
+Normal
+H2-remap
+------------------------------ */
